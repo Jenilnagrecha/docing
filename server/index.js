768ad73ba4f3b4197ba6bd1330f1924e01176cf4
jenilnagrecha <nagrechajenil@gmail.com>
2023-03-03 16:17:35 +0530

    connected with DB

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+const DB =
+  "mongodb+srv://nagrechajenil:WeMi59QThUzVFApM@cluster0.fysgzcq.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(DB)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
